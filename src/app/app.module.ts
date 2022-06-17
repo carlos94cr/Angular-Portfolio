d75162be2329011cb8a70a9520a6dc8f8467ba6b
carlos94cr <carlos94cr@gmail.com>
2022-06-17 19:34:15 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { HeaderComponent } from './header/header.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroComponent,
+    HeaderComponent,
+    ProjectsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
